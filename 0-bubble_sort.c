59d5c049e3fe71aab728a7703d802a9f36162902
Maryqueen123 <maryqueenchy@gmail.com>
2023-04-25 14:32:01 -0700

    tasks 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include "sort.h"
 
-void bubble_sort(int *array, size_t size) {
+void bubble_sort(int *array, size_t size){
     int i, j, temp;
-    for (i = 0; i < size - 1; i++) {
-        for (j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j+1]) {
+    for (i = 0; i < size - 1; i++){
+        for (j = 0; j < size - i - 1; j++){
+            if (array[j] > array[j+1]){
                 // swap array[j] and array[j+1]
                 temp = array[j];
                 array[j] = array[j+1];
                 array[j+1] = temp;
-            }
+	    }
         }
     }
 }
